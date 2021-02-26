10721d892ae4ca693d31e51de9b3aa86df68a8c9
Anders Lauvrak Espenes <andersespenes@hotmail.com>
2021-02-26 16:07:17 +0100

    Fixed piano key length, check for correct color and change to white or black on creation

--- piano/app/src/main/java/com/example/piano/WhiteKey.kt
+++ piano/app/src/main/java/com/example/piano/WhiteKey.kt
@@ -2,10 +2,13 @@ package com.example.piano
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.piano.databinding.FragmentWhiteKeyBinding
 import kotlinx.android.synthetic.main.fragment_white_key.view.*
@@ -16,6 +19,7 @@ class WhiteKey : Fragment() {
     private var _binding:FragmentWhiteKeyBinding? = null // Note: FragmentWhiteKeyBinding is a automaticly generated class based on the xlm file fragment_white_key, name scheme is mandatory
     private val binding get() = _binding!!
     private lateinit var note:String // Contains the note this object will represent
+    private var black: Boolean = false// True if key should be black
 
 
     // Declare functions, must be defined by parent class
@@ -29,14 +33,17 @@ class WhiteKey : Fragment() {
         }
     }
 
-    @SuppressLint("ClickableViewAccessibility")
+    //@SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentWhiteKeyBinding.inflate(inflater)
+        _binding = FragmentWhiteKeyBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        if(black)
+            view.whiteKeyButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.black)
 
         view.whiteKeyButton.setOnTouchListener { _, event ->
             when (event?.action) {  // on touch event get touch event down and up and invoke function
@@ -45,6 +52,8 @@ class WhiteKey : Fragment() {
             }
             true
         }
+
+
         // Inflate the layout for this fragment
         return view
     }
@@ -55,6 +64,10 @@ class WhiteKey : Fragment() {
             WhiteKey().apply {
                 arguments = Bundle().apply {
                     putString("NOTE", note)
+                    if(note.contains("#")) {
+                        black = true
+                    }
+
                 }
             }
     }
